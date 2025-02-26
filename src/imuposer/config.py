846b8b6362baf93e7441d0e08c888f5a869c394f
zahrayasmne123 <132264334+zahrayasmne123@users.noreply.github.com>
2025-02-26 20:19:29 +0000

    FIX: move smpl file

--- src/imuposer/config.py
+++ src/imuposer/config.py
@@ -38,7 +38,7 @@ class Config:
     
     def build_paths(self):
         self.smpl_model_path = self.root_dir / "src/imuposer/smpl/model.pkl"
-        self.og_smpl_model_path = self.root_dir / "src/imuposer/smpl/basicmodel_m_lbs_10_207_0_v1.0.0.pkl"
+        self.og_smpl_model_path = Path("/content/IMUPoser/src/imuposer/smpl/basicmodel_m_lbs_10_207_0_v1.0.0.pkl")
         
          
         self.raw_dip_path = Path("/content/drive/MyDrive/data/DIP_IMU")
