e3292f6f158d0c7dfbd19b0786de81e1a5991ef2
Rahman Zahra <zahra.rahman.1@warwick.ac.uk>
2025-05-06 17:28:05 +0100

    dfs -> dataframes

--- preprocessing/csvtotensor.py
+++ preprocessing/csvtotensor.py
@@ -5,7 +5,7 @@ import torch
 data to tensors to be put in the IMUPoser deep learning algorithm  
 
 
-1.Validate_input_data: Input list of (dfs) and corresponding device name to make a pair
+1.Validate_input_data: Input list of (dataframes) and corresponding device name to make a pair
     If there is a valid df, checks each dataframe to ensure all expected columns are present, if missing raise an error
     Check timestamp consistency and prints confirmation messages if validation passes
 2.Df_to_numpy_array: Extracts accelerometer and rotation matrix data combining them into a single numpy array 
@@ -14,10 +14,10 @@ data to tensors to be put in the IMUPoser deep learning algorithm
 
 """
 
-def validate_input_data(dfs, device_names):
+def validate_input_data(dataframes, device_names):
     # Checks if there's at least one valid dataframe, error if noy
-    df_device_names = [(df, name) for df, name in zip(dfs, device_names) if df is not None]  
-    valid_dfs = [pair[0] for pair in df_device_names]
+    df_device_names = [(df, name) for df, name in zip(dataframes, device_names) if df is not None]  
+    valid_dataframes = [pair[0] for pair in df_device_names]
     valid_device_names = [pair[1] for pair in df_device_names]
     
     # Check for expected columns in each dataframe
@@ -25,22 +25,22 @@ def validate_input_data(dfs, device_names):
                    [f'R{i}{j}' for i in range(3) for j in range(3)]
     
     # Reports on missing columns 
-    for df, name in zip(valid_dfs, valid_device_names):
+    for df, name in zip(valid_dataframes, valid_device_names):
         missing_cols = set(valid_column_names) - set(df.columns)
         if missing_cols:
             raise ValueError(f"{name} is missing columns: {missing_cols}")
     
     # Counts the number of rows in each dataframe checks all df have same number of rows
-    row_counter = [len(df) for df in valid_dfs]
+    row_counter = [len(df) for df in valid_dataframes]
     if len(set(row_counter)) != 1:
         raise ValueError("Inconsistent number of rows")
     
     # Check timestamp consistency
-    base_timestamps = valid_dfs[0]['timestamp'].values
-    for df, name in zip(valid_dfs[1:], valid_device_names[1:]):
+    base_timestamps = valid_dataframes[0]['timestamp'].values
+    for df, name in zip(valid_dataframes[1:], valid_device_names[1:]):
         if not np.array_equal(base_timestamps, df['timestamp'].to_numpy()):
             raise ValueError(f"Timestamps don't match between {valid_device_names[0]} and {name}")
-    return row_counter[0], valid_dfs, valid_device_names
+    return row_counter[0], valid_dataframes, valid_device_names
 
 
 def df_to_numpy_array(df):
@@ -50,11 +50,11 @@ def df_to_numpy_array(df):
 
 
 
-def create_IMUPoser_tensor(dfs_list, device_names,output_path):
+def create_IMUPoser_tensor(dataframes_list, device_names,output_path):
     #Validate input data
-    valid_names = [name for df, name in zip(dfs_list, device_names) if df is not None]
-    valid_dfs_list = [df for df in dfs_list if df is not None]
-    n_frames, valid_dfs, valid_device_names = validate_input_data(valid_dfs_list, valid_names)
+    valid_names = [name for df, name in zip(dataframes_list, device_names) if df is not None]
+    valid_dataframes_list = [df for df in dataframes_list if df is not None]
+    n_frames, valid_dataframes, valid_device_names = validate_input_data(valid_dataframes_list, valid_names)
     
     # Define standard device mapping according to IMUPoser
     device_positions = {
@@ -66,7 +66,7 @@ def create_IMUPoser_tensor(dfs_list, device_names,output_path):
     
     # Initialise tensor with zeros (n_frames x 60) and process each device in the correct position
     tensor_data = np.zeros((n_frames, 60))
-    for df, name in zip(valid_dfs, valid_device_names):
+    for df, name in zip(valid_dataframes, valid_device_names):
         position = None
         
         # Try to match the device name directly
